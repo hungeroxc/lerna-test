c1d77388497439158f7199c2cfbefd9627da1780
tanzedong <zedong@bihu.it>
2020-09-07 09:58:22 +0800

    feat: 123
    affects: task
    
    13
    
    BREAKING CHANGE:
    123
    
    ISSUES CLOSED: #123

--- repos/task/src/App.vue
+++ repos/task/src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <div id="app">
+  <div @click="onClick" id="app">
     <HelloWorld/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// 测试
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    onClick() {
+      console.log(123)
+    }
   }
 }
 </script>
